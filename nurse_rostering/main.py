fb31d203cf5f25eace74ffbc872592c9aef1c480
antoine-agre <80821251+antoine-agre@users.noreply.github.com>
2024-01-16 17:21:20 +0100

    feat: implement importing instance from .txt file

--- nurse_rostering/main.py
+++ nurse_rostering/main.py
@@ -1,7 +1,11 @@
 class Staff:
     
-    def __init__(self, min_worktime: int, max_worktime: int, min_consecutive_shifts: int, max_consecutive_shifts: int, min_consecutive_rest_days: int, max_worked_weekends: int):
+    def __init__(self, days_count: int, shift_count: int, id: str, 
+                 min_worktime: int, max_worktime: int, 
+                 min_consecutive_shifts: int, max_consecutive_shifts: int, 
+                 min_consecutive_rest_days: int, max_worked_weekends: int):
         
+        self.id: str = id
         self.rest_days: list[int] = []
         self.min_worktime: int = min_worktime
         self.max_worktime: int = max_worktime
@@ -12,21 +16,55 @@ class Staff:
         # Pour chaque poste, le nombre de jour maximum où cet employé peut y être assigné
         self.max_shift_days: list[int] = []
         # Pour chaque jour et poste, la pénalité si l'employé ni est pas assigné
-        self.shift_wish_penalties: list[list[int]] = []
+        self.shift_wish_penalties: list[list[int|None]] = [[None for _ in range(shift_count)] for _ in range(days_count)]
         # Pour chaque jour et poste, la pénalité si l'employé y est assigné
-        self.shift_avoid_penalties: list[list[int]] = []
+        self.shift_avoid_penalties: list[list[int|None]] = [[None for _ in range(shift_count)] for _ in range(days_count)]
+
+    def __str__(self) -> str:
+        out = f"Staff {self.id} :"
+        out += f"\n\tRest days : {self.rest_days}"
+        out += f"\n\tMin/Max Worktime : {self.min_worktime}/{self.max_worktime}"
+        out += f"\n\tMin/Max Consecutive shifts : {self.min_consecutive_shifts}/{self.max_consecutive_shifts}"
+        out += f"\n\tMin Consecutive rest days : {self.min_consecutive_rest_days}"
+        out += f"\n\tMax worked weekends : {self.max_worked_weekends}"
+        out += f"\n\tMax shift days :"
+        for i, max_shift_day in enumerate(self.max_shift_days):
+            out += f"\n\t\t[{i}] : {max_shift_day}"
+        out += f"\n\tShift Wish Penalties : {self.shift_wish_penalties}"
+        out += f"\n\tShift Avoid Penalties : {self.shift_avoid_penalties}"
+        return out
 
 class ShiftType:
     
-    def __init__(self, duration: int):
+    def __init__(self, id: str, duration: int):
+        # Chaîne de caractères identifiant le shift type
+        self.id: str = id
+        # Durée du shift en minutes
         self.duration: int = duration
-        self.blocked_shift_types: list[ShiftType] = []
+        # Liste de ShiftTypes qui ne peuvent pas être pris après celui-ci
+        # self.blocked_shift_types: list[ShiftType] = []
+        self.blocked_shift_types: list[int] = []
         # Pour chaque jour, représente le nombre d'employés requis.
         self.staff_requirements: list[int] = []
         # Pour chaque jour, pénalité par employé au-dessus du nombre requis
         self.cover_above_penalties: list[int] = []
         # Pour chaque jour, pénalité par employé en-dessous du nombre requis
         self.cover_below_penalties: list[int] = []
+    
+    def __eq__(self, __value: object) -> bool:
+        if type(__value) != ShiftType:
+            return False
+        else:
+            return self.id == __value.id
+    
+    def __str__(self):
+        out = f"Shift {self.id} : {self.duration} minutes\n\tBlocked shifts : "
+        for blocked_shift in self.blocked_shift_types:
+            out += str(blocked_shift) + ", "
+        out += f"\n\tStaff requirements : {self.staff_requirements}"
+        out += f"\n\tCover above penalties : {self.cover_above_penalties}"
+        out += f"\n\tCover below penalties : {self.cover_below_penalties}"
+        return out
 
 class Problem:
 
@@ -34,13 +72,34 @@ class Problem:
         self.days_count: int = days_count
         self.staff: list[Staff] = []
         self.shift_types: list[ShiftType] = []
+    
+    def shift_type_from_id(self, shift_id: str)-> ShiftType|None:
+        for shift_type in self.shift_types:
+            if shift_type.id == shift_id:
+                return shift_type
+        return None
 
-        self.staff_dict: dict[str, int] = {}
-        self.shift_dict: dict[str, int] = {}
+    def shift_int_from_id(self, shift_id: str)-> int|None:
+        for i in range(len(self.shift_types)):
+            if self.shift_types[i].id == shift_id:
+                return i
+        return None
+    
+    def staff_int_from_id(self, staff_id: str)-> int|None:
+        for i in range(len(self.staff)):
+            if self.staff[i].id == staff_id:
+                return i
+        return None
+    
+    # def __str__(self) -> str:
+    #     out = ""
+    #     for i, shift_type in enumerate(self.shift_types):
+    #         out += self.shift_dict[i] + " " + shift_type
+    #     return out
 
 # Exemple basé sur l'instance 1
-problem: Problem = Problem(14)
-problem.shift_types.append(ShiftType(480))
-staff = Staff(max_worktime=4320, min_worktime=3360, max_consecutive_shifts=5, min_consecutive_shifts=2, min_consecutive_rest_days=2, max_worked_weekends=1)
-staff.max_shift_days = [14]
-problem.staff.append(staff)
+# problem: Problem = Problem(14)
+# problem.shift_types.append(ShiftType(480))
+# staff = Staff(max_worktime=4320, min_worktime=3360, max_consecutive_shifts=5, min_consecutive_shifts=2, min_consecutive_rest_days=2, max_worked_weekends=1)
+# staff.max_shift_days = [14]
+# problem.staff.append(staff)
