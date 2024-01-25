fb04564bcb9887fc496d2255e6049c598bc5689d
antoine-agre <80821251+antoine-agre@users.noreply.github.com>
2024-01-25 23:03:02 +0100

    refactor: split Solution.is_feasible into individual function
    Takes advantage of the fact that hard constraints are only checked individually, i.e. a solution is feasible if and only if the schedule of every staff member is feasible.

--- nurse_rostering/model/solution.py
+++ nurse_rostering/model/solution.py
@@ -31,94 +31,13 @@ class Solution:
     def is_feasible(self)-> bool:
         """Indicates wether the solution's hard constraints are respected."""
 
-        # Check -1 values
-        for schedule in self.planning:
-            for day in schedule:
-                if day == -1:
-                    return False
-
-        # Blocking shifts
-        for schedule in self.planning:
-            for d in range(1, len(schedule)):
-                current_shift = schedule[d]
-                previous_shift = schedule[d - 1]
-                if current_shift != None and previous_shift != None:
-                    if current_shift in self.problem.shift_types[previous_shift].blocked_shift_types:
-                        # print("Blocking shifts")
-                        return False
-            
-        # Max shift
-        for staff_int in range(len(self.planning)):
-            schedule = self.planning[staff_int]
-            max_shift_days = self.problem.staff[staff_int].max_shift_days
-            shift_count = [0 for _ in range(len(max_shift_days))]
-            for day in schedule:
-                if day != None:
-                    shift_count[day] += 1
-            for i in range(len(shift_count)):
-                if shift_count[i] > max_shift_days[i]:
-                    # print("Max shifts")
-                    return False
-        
-        # Maximum and minimum minutes worked
-        for staff_int in range(len(self.planning)):
-            schedule = self.planning[staff_int]
-            minutes = 0
-            for day in schedule:
-                if day != None:
-                    minutes += self.problem.shift_types[day].duration
-            staff = self.problem.staff[staff_int]
-            if minutes > staff.max_worktime or minutes < staff.min_worktime:
-                # print("max/min minutes worked")
+        for staff_int in range(len(self.problem.staff)):
+            schedule: PersonnalSchedule = self.planning[staff_int]
+            if is_personal_schedule_feasible(schedule, self.problem, staff_int) == False:
                 return False
         
-        # Max/min consecutive shifts and min consecutive days off
-        for staff_int in range(len(self.planning)):
-            staff = self.problem.staff[staff_int]
-            schedule = self.planning[staff_int].copy()
-            while len(schedule) > 0:
-                content_type = type(schedule.pop(0))
-                count = 1
-                while len(schedule) > 0 and type(schedule[0]) == content_type:
-                    schedule.pop(0)
-                    count += 1
-                
-                if content_type == type(None):
-                    if count < staff.min_consecutive_rest_days:
-                        # print("min consecutive rest days")
-                        return False
-                else:
-                    if count < staff.min_consecutive_shifts or count > staff.max_consecutive_shifts:
-                        # print("content :", content_type, "count :", count)
-                        # print(f"[staff {staff_int}] max/min consecutive shifts")
-                        return False
-        
-        # Max number of weekends
-        for staff_int in range(len(self.planning)):
-            staff = self.problem.staff[staff_int]
-            schedule = self.planning[staff_int]
-            max_weekends = staff.max_worked_weekends
-            weekends = []
-            for i in range(len(schedule)//7):
-                weekends.append((schedule[7*i+5], schedule[7*i+6]))
-            for weekend in weekends:
-                if weekend[0] != None or weekend[1] != None:
-                    max_weekends -= 1
-            if max_weekends < 0:
-                # print("max weekends worked")
-                return False
-        
-        # Requested days off
-        for staff_int in range(len(self.planning)):
-            staff = self.problem.staff[staff_int]
-            schedule = self.planning[staff_int]
-            for rest_day in staff.rest_days:
-                if schedule[rest_day] != None:
-                    # print("requested days off")
-                    return False
-
         return True
-    
+
     def value(self):
         
         cover_abovePenality = 0
@@ -207,7 +126,87 @@ class Solution:
                 # if schedule == None: break
                 self.planning[staff_int] = schedule
             
+
+def is_personal_schedule_feasible(schedule: PersonnalSchedule, 
+                                  problem: Problem, staff_int: int) -> bool:
+    """Indicates wether the personal schedule's hard constraints are respected."""
     
+    # Variables
+    staff: Staff = problem.staff[staff_int]
+
+    # Check -1 values
+    for day in schedule:
+        if day == -1:
+            return False
+
+    # Blocking shifts
+    for d in range(1, len(schedule)):
+        current_shift = schedule[d]
+        previous_shift = schedule[d - 1]
+        if current_shift != None and previous_shift != None:
+            if current_shift in problem.shift_types[previous_shift].blocked_shift_types:
+                # print("Blocking shifts")
+                return False
+        
+    # Max shift
+    max_shift_days = staff.max_shift_days
+    shift_count = [0 for _ in range(len(max_shift_days))]
+    for day in schedule:
+        if day != None:
+            shift_count[day] += 1
+    for i in range(len(shift_count)):
+        if shift_count[i] > max_shift_days[i]:
+            # print("Max shifts")
+            return False
+    
+    # Maximum and minimum minutes worked
+    minutes = 0
+    for day in schedule:
+        if day != None:
+            minutes += problem.shift_types[day].duration
+    if minutes > staff.max_worktime or minutes < staff.min_worktime:
+        # print("max/min minutes worked")
+        return False
+    
+    # Max/min consecutive shifts and min consecutive days off
+    schedule_copy = deepcopy(schedule)
+
+    while len(schedule_copy) > 0:
+        content_type = type(schedule_copy.pop(0))
+        count = 1
+        while len(schedule_copy) > 0 and type(schedule_copy[0]) == content_type:
+            schedule_copy.pop(0)
+            count += 1
+        
+        if content_type == type(None) and count < staff.min_consecutive_rest_days:
+            # print("min consecutive rest days")
+            return False
+        elif count < staff.min_consecutive_shifts or count > staff.max_consecutive_shifts:
+            # print("content :", content_type, "count :", count)
+            # print(f"[staff {staff_int}] max/min consecutive shifts")
+            return False
+    
+    # Max number of weekends
+    max_weekends = staff.max_worked_weekends
+    weekends = []
+    for i in range(len(schedule)//7):
+        weekends.append((schedule[7*i+5], schedule[7*i+6]))
+    for weekend in weekends:
+        if weekend[0] != None or weekend[1] != None:
+            max_weekends -= 1
+    if max_weekends < 0:
+        # print("max weekends worked")
+        return False
+    
+    # Requested days off
+    for rest_day in staff.rest_days:
+        if schedule[rest_day] != None:
+            # print("requested days off")
+            return False
+
+    return True
+
+
 def set_days_off(problem: Problem, staff: Staff, schedule: PersonnalSchedule) -> PersonnalSchedule:
     for d in range(problem.days_count):
         if d not in staff.rest_days:
