5bd845b06188052c227e0c2e15d297c475227ad8
Giovanni <alllaglo.j@gmail.com>
2024-01-27 10:06:36 +0100

    Update the file

--- nurse_rostering/__main__.py
+++ nurse_rostering/__main__.py
@@ -2,25 +2,7 @@ from nurse_rostering.model.solution import Solution
 from nurse_rostering.io.export_solution import Solution2file
 from nurse_rostering.io.importer import Importer
 from datetime import datetime
-import time
 
-infos = [ ("SchedulingPeriodFile","./Instance3.ros"), # chemin absolu | chemin relatif par rapport au fichier solution.
-        ("Penalty","521"),
-        ("DateFound","12/10/2023"),
-        ("FoundBy","Arthur"),
-        ("System", "Windows 10 pro"),
-        ("CPU","processeur Intel Core i5 2.48Ghz"),
-        ("Algorithm","VNS"),
-        ("CpuTime","1235.02") ]
-
-problem = Importer().import_problem("Instance3.txt")
-print(problem)
-a = Solution.from_problem(problem)
-
-print(a.planning)
-
-s2f = Solution2file(problem, a.planning, infos, "Instance3")
-s2f.generate_rosterFile(".")
 
 
 def info_provider(solution):
@@ -45,3 +27,17 @@ def info_provider(solution):
                         ("CpuTime",cpu_time) ]
         
         return informations
+
+###
+
+problem = Importer().import_problem("Instance3.txt")
+print(problem)
+a = Solution.from_problem(problem)
+
+print(a.planning)
+
+##### SET THE a.pathTowardsProblem: str BEFORE RUNNING ####
+infos =  info_provider(a)
+
+s2f = Solution2file(problem, a.planning, infos)
+s2f.generate_rosterFile("nurse_rostering/examples/solutions/")
--- nurse_rostering/io/export_solution.py
+++ nurse_rostering/io/export_solution.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as ET
-import os
 from nurse_rostering.model.problem import Problem, Staff, ShiftType
 
 # A Verifier
@@ -22,14 +21,13 @@ from nurse_rostering.model.problem import Problem, Staff, ShiftType
 
 class Solution2file:
 
-    def __init__(self, problem:Problem, planning:list[list[int]], info:list[tuple[str,str]], name):
+    def __init__(self, problem:Problem, planning:list[list[int]], info:list[tuple[str,str]]):
         
         self.int2staffID = [staff.id for staff in problem.staff]
         # shift index in the list to match shiftId in the problem
         self.int2shiftID = [shiftType.id for shiftType in problem.shift_types]
         self.info = info # list of tuple (property,value) of informations about problem resolution 
         self.planning = planning # matrix of solution
-        self.name = name
 
     def __info__(self):
         print("Informations about solution processing:")
@@ -41,12 +39,9 @@ class Solution2file:
     
         #root of xml (.ros file)
         roster = ET.Element('Roster',attrib = {"xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance","xsi:noNamespaceSchemaLocation":"Roster.xsd"})
-        
         #input info about problem resolution
-
-        relativePath =  os.path.relpath(self.info[0][1], baseFolder) # relative path
         element = ET.SubElement(roster,self.info[0][0])
-        element.text = relativePath
+        element.text = self.info[0][1]
 
         for item in self.info[1:]:
             element = ET.SubElement(roster,item[0])
@@ -76,24 +71,12 @@ class Solution2file:
         
         #save file
         penality  = self.info[1][1]
-        solutionFilename : str = findFilenameWithoutExtension(self.info[0][1])+".Solution."+self.name+".ros"
+        solutionFilename : str = findFilenameWithoutExtension(self.info[0][1])+".Solution."+penality+".ros"
         solutionPath = baseFolder+solutionFilename
         fileProvider.write(solutionPath,encoding="UTF-8",xml_declaration= True)
 
         
 def findFilenameWithoutExtension(path):
 
-    folderSep = os.path.sep
-    filename = path.split(folderSep)[-1]
+    filename = path.split("/")[-1]
     return filename.split(".")[0]
-
-if __name__ == "__main__":
-
-    infos = [ ("SchedulingPeriodFile","/nurse_rostering/examples/Instance1.ros"), # chemin absolu | chemin relatif par rapport au fichier solution.
-        ("Penalty","521"),
-        ("DateFound","12/10/2023"),
-        ("FoundBy","Arthur"),
-        ("System", "Windows 10 pro"),
-        ("CPU","processeur Intel Core i5 2.48Ghz"),
-        ("Algorithm","VNS"),
-        ("CpuTime","1235.02") ]
